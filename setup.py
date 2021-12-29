746be8710cb6b5629e381c2d253a2b9a0513b4ec
Mehdi Yaqoubi <mehdi.code@pm.me>
2021-12-29 17:54:25 +0330

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='fixer-demo',
+      version='0.1',
+      description='Fixer service demo package',
+      url='https://github.com/MehdiYaqoubi/fixer-demo.git',
+      author='Mehdi Yaqoubi',
+      author_email='mehdi.code@pm.me',
+      license='MIT',
+      packages=['fixer'],
+      zip_safe=False)
